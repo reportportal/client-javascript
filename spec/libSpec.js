1d9a99df2208617d6eac0161ca085fb56784c0f1
Mikhail Subach <scorpibear@tut.by>
2017-06-20 14:39:14 +0300

    startLaunch(), updateLauchDescription(), getResponsePromise() tests added

--- spec/libSpec.js
+++ spec/libSpec.js
@@ -33,4 +33,40 @@ describe("ReportPortal javascript client", function() {
       expect(new Date() - client._now()).toBeLessThan(100); // less than 100 miliseconds difference
     });
   });
+  describe("startLaunch", function() {
+    it("calls getResponsePromise", function() {
+      client = new RPClient({token: "startLaunchTest", endpoint: "https://rp.us/api/v1", project:"tst"});
+      myPromise = new Promise(()=>{});
+      spyOn(client, '_getResponsePromise').and.returnValue(myPromise);
+
+      client.startLaunch({});
+
+      expect(client._getResponsePromise).
+        toHaveBeenCalledWith("https://rp.us/api/v1/tst/launch", {}, {headers: client.headers}, "POST");
+    });
+  });
+  describe("updateLaunchDescription", function() {
+    it("sends put request to update description for launch", function() {
+      client = new RPClient({token: "upLaunchDescTest", endpoint: "https://rp.us/api/v1", project:"tst"});
+      spyOn(client, '_getResponsePromise');
+
+      client.updateLaunchDescription("id5", "newone");
+
+      expect(client._getResponsePromise).
+        toHaveBeenCalledWith("https://rp.us/api/v1/tst/launch/id5/update",
+        {description: "newone"},
+        {headers: client.headers}, "PUT");
+    });
+  });
+  describe("_getResponsePromise", function() {
+    it("creates promise to send json via rest", function(done) {
+      let rest = require('restler');
+      spyOn(rest, 'json');
+
+      client._getResponsePromise("url", {param: "value"}, "options", "method");
+
+      expect(rest.json).toHaveBeenCalledWith("url", {param:"value"}, "options", "method");
+      done(); // need as async code is checked
+    });
+  });
 });
