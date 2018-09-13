6cd55812f6c3c914ecc9ee0960b26192fccd304f
Boris Osipov <osipov.boris@gmail.com>
2018-09-14 00:13:25 +0300

    Add linter. Fix linter errors.

--- examples/testConnect.js
+++ examples/testConnect.js
@@ -1,15 +1,16 @@
-var RPClient = require('../lib/report-portal-client.js');
+/* eslint-disable no-console */
+const RPClient = require('../lib/report-portal-client.js');
 
-var rpClient = new RPClient({
-    token: "00000000-0000-0000-0000-000000000000",
-    endpoint: "http://your-instance.com:8080/api/v1",
-    launch: "LAUNCH_NAME",
-    project: "PROJECT_NAME"
+const rpClient = new RPClient({
+    token: '00000000-0000-0000-0000-000000000000',
+    endpoint: 'http://your-instance.com:8080/api/v1',
+    launch: 'LAUNCH_NAME',
+    project: 'PROJECT_NAME',
 });
 
-rpClient.checkConnect().then((responce) => {
+rpClient.checkConnect().then((response) => {
     console.log('You have successfully connected to the server.');
-    console.log(`You are using an account: ${responce.full_name}`);
+    console.log(`You are using an account: ${response.full_name}`);
 }, (error) => {
     console.log('Error connection to server');
     console.dir(error);
