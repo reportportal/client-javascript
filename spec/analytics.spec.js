82ebfb1d74741d3b8c6ce3bf68871a988f9c70a4
Ilya Hancharyk <Ilya_Hancharyk@epam.com>
2022-01-12 18:02:50 +0300

    Use 2 space indentation

--- spec/analytics.spec.js
+++ spec/analytics.spec.js
@@ -2,45 +2,45 @@ const Analytics = require('../analytics/analytics');
 const { getAgentEventLabel } = require('../analytics/events');
 
 const agentParams = {
-    name: 'AgentName',
-    version: 'AgentVersion',
+  name: 'AgentName',
+  version: 'AgentVersion',
 };
 
 describe('Analytics', () => {
-    describe('class Analytics', () => {
-        let analytics;
+  describe('class Analytics', () => {
+    let analytics;
 
-        beforeEach(() => {
-            analytics = new Analytics(agentParams);
-        });
-
-        it('should be properly initialized', () => {
-            expect(analytics.agentParams).toEqual(agentParams);
-            expect(analytics.visitorInstance).toBeDefined();
-        });
-
-        it('trackEvent should called the method event with correct parameters', () => {
-            spyOn(analytics.visitorInstance, 'event').and.returnValue({
-                send: () => {},
-            });
-
-            analytics.trackEvent({
-                category: 'category',
-                action: 'action',
-                label: 'label',
-            });
-
-            expect(analytics.visitorInstance.event).toHaveBeenCalledWith('category', 'action', 'label');
-        });
+    beforeEach(() => {
+      analytics = new Analytics(agentParams);
     });
 
-    describe('events', () => {
-        it('getAgentEventLabel return correct label', () => {
-            const expectedLabel = 'Agent name "AgentName", version "AgentVersion"';
-
-            const label = getAgentEventLabel(agentParams);
-
-            expect(label).toEqual(expectedLabel);
-        });
+    it('should be properly initialized', () => {
+      expect(analytics.agentParams).toEqual(agentParams);
+      expect(analytics.visitorInstance).toBeDefined();
     });
+
+    it('trackEvent should called the method event with correct parameters', () => {
+      spyOn(analytics.visitorInstance, 'event').and.returnValue({
+        send: () => {},
+      });
+
+      analytics.trackEvent({
+        category: 'category',
+        action: 'action',
+        label: 'label',
+      });
+
+      expect(analytics.visitorInstance.event).toHaveBeenCalledWith('category', 'action', 'label');
+    });
+  });
+
+  describe('events', () => {
+    it('getAgentEventLabel return correct label', () => {
+      const expectedLabel = 'Agent name "AgentName", version "AgentVersion"';
+
+      const label = getAgentEventLabel(agentParams);
+
+      expect(label).toEqual(expectedLabel);
+    });
+  });
 });
