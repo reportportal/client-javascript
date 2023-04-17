301dbbae48f199c13dd8938211e342688936fd39
Vadzim Hushchanskou <vadzim_hushchanskou@epam.com>
2023-04-17 21:24:14 +0300

    Refactoring

--- statistics/clientId.js
+++ statistics/clientId.js
@@ -1,55 +1,36 @@
 // client-id.js
 const fs = require('fs');
-const path = require('path');
-const os = require('os');
 const ini = require('ini');
 const { v4: uuidv4 } = require('uuid');
-const { ENCODING, CLIENT_ID_KEY, RP_FOLDER, RP_PROPERTIES_FILE } = require('./constants');
-
-function getRpFolderPath() {
-  return path.join(os.homedir(), RP_FOLDER);
-}
-
-function getRpPropertiesFilePath() {
-  return path.join(getRpFolderPath(), RP_PROPERTIES_FILE);
-}
+const { ENCODING, CLIENT_ID_KEY, RP_FOLDER_PATH, RP_PROPERTIES_FILE_PATH } = require('./constants');
 
 function readClientId() {
-  const propertiesFilePath = getRpPropertiesFilePath();
-
-  if (fs.existsSync(propertiesFilePath)) {
-    const propertiesContent = fs.readFileSync(propertiesFilePath, ENCODING);
+  if (fs.existsSync(RP_PROPERTIES_FILE_PATH)) {
+    const propertiesContent = fs.readFileSync(RP_PROPERTIES_FILE_PATH, ENCODING);
     const properties = ini.parse(propertiesContent);
     return properties[CLIENT_ID_KEY];
   }
-
   return null;
 }
 
 function storeClientId(clientId) {
-  const propertiesFilePath = getRpPropertiesFilePath();
   const properties = {};
-
-  if (fs.existsSync(propertiesFilePath)) {
-    const propertiesContent = fs.readFileSync(propertiesFilePath, ENCODING);
+  if (fs.existsSync(RP_PROPERTIES_FILE_PATH)) {
+    const propertiesContent = fs.readFileSync(RP_PROPERTIES_FILE_PATH, ENCODING);
     Object.assign(properties, ini.parse(propertiesContent));
   }
-
   properties[CLIENT_ID_KEY] = clientId;
   const propertiesContent = ini.stringify(properties);
-
-  fs.mkdirSync(getRpFolderPath(), { recursive: true });
-  fs.writeFileSync(propertiesFilePath, propertiesContent, ENCODING);
+  fs.mkdirSync(RP_FOLDER_PATH, { recursive: true });
+  fs.writeFileSync(RP_PROPERTIES_FILE_PATH, propertiesContent, ENCODING);
 }
 
 function getClientId() {
   let clientId = readClientId();
-
   if (!clientId) {
     clientId = uuidv4(undefined, undefined, 0);
     storeClientId(clientId);
   }
-
   return clientId;
 }
 
--- statistics/constants.js
+++ statistics/constants.js
@@ -1,3 +1,5 @@
+const os = require('os');
+const path = require('path');
 const Bowser = require('bowser');
 const pjson = require('../package.json');
 
@@ -7,6 +9,8 @@ const PJSON_NAME = pjson.name;
 const CLIENT_ID_KEY = 'client.id';
 const RP_FOLDER = '.rp';
 const RP_PROPERTIES_FILE = 'rp.properties';
+const RP_FOLDER_PATH = path.join(os.homedir(), RP_FOLDER);
+const RP_PROPERTIES_FILE_PATH = path.join(RP_FOLDER_PATH, RP_PROPERTIES_FILE);
 const CLIENT_INFO = atob('Ry1XUDU3UlNHOFhMOmVFazhPMGJ0UXZ5MmI2VXVRT19TOFE=');
 const [MEASUREMENT_ID, API_KEY] = CLIENT_INFO.split(':');
 const EVENT_NAME = 'start_launch';
@@ -52,7 +56,9 @@ module.exports = {
   PJSON_NAME,
   CLIENT_ID_KEY,
   RP_FOLDER,
+  RP_FOLDER_PATH,
   RP_PROPERTIES_FILE,
+  RP_PROPERTIES_FILE_PATH,
   MEASUREMENT_ID,
   API_KEY,
   INTERPRETER,
