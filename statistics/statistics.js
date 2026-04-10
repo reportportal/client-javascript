f1860f70c5cb625c4921ad4ea104e7c44c7cfaa6
maria-hambardzumian <maria_hambardzumyan@gmail.com>
2026-04-10 10:38:57 +0400

    EPMRPP-113919 || lint fix

--- statistics/statistics.js
+++ statistics/statistics.js
@@ -24,7 +24,11 @@ class Statistics {
     if (agentParams && hasOption(agentParams, 'version') && agentParams.version) {
       params.agent_version = agentParams.version;
     }
-    if (agentParams && hasOption(agentParams, 'framework_version') && agentParams.framework_version) {
+    if (
+      agentParams &&
+      hasOption(agentParams, 'framework_version') &&
+      agentParams.framework_version
+    ) {
       params.framework_version = agentParams.framework_version;
     }
     return params;
