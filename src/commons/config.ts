47b252060eda9fb06d0a69e2cfc1ada47555f85a
maria-hambardzumian <maria_hambardzumyan@epam.com>
2026-09-01 13:27:43 +0400

    Preserve valid config options when getClientConfig fails

--- src/commons/config.ts
+++ src/commons/config.ts
@@ -83,7 +83,10 @@ const DEFAULT_CLIENT_CONFIG: NormalizedClientConfig = {
 };
 
 export const getClientConfig = (options: ReportPortalConfig): NormalizedClientConfig => {
-  let calculatedOptions = DEFAULT_CLIENT_CONFIG;
+  let calculatedOptions: NormalizedClientConfig =
+    options && typeof options === 'object'
+      ? ({ ...DEFAULT_CLIENT_CONFIG, ...options } as NormalizedClientConfig)
+      : DEFAULT_CLIENT_CONFIG;
   try {
     if (typeof options !== 'object') {
       throw new ReportPortalValidationError('`options` must be an object.');
